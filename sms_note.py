406a3287b8634184d2d0fff5126d6e2f94a8ad3c
meena575 <107091169+meena575@users.noreply.github.com>
2024-08-13 20:18:53 +0530

    Update sms_note.py

--- sms_note.py
+++ sms_note.py
@@ -5,8 +5,8 @@ from twilio.rest import Client
 
 def send_sms_notification(to, message):
     # Your Twilio account SID and Auth Token
-    account_sid = 'ACe60f9d116abaa8c2bdc10ffca500a06b'
-    auth_token = 'd85768ddc47415e4667848856aacdeee'
+    account_sid = 'place your sid here'
+    auth_token = 'place your token here'
     client = Client(account_sid, auth_token)
 
     # Send SMS
@@ -19,8 +19,8 @@ def send_sms_notification(to, message):
     print(f"Message sent to {to}: {message.sid}")
 def send_sms_notification_multi(to_numbers, message):
     # Your Twilio account SID and Auth Token
-    account_sid = 'ACe60f9d116abaa8c2bdc10ffca500a06b'
-    auth_token = 'd85768ddc47415e4667848856aacdeee'
+    account_sid = 'place your sid here'
+    auth_token = 'place your token here'
     client = Client(account_sid, auth_token)
 
     # Send SMS to each number in the list
